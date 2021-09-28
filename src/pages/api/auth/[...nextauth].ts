fb83085a7fbd171e9e7c8cd2643c8f563ddff6b5
Paulo Xavier <pxavierf@gmail.com>
2021-09-28 20:00:17 -0300

    cconnection with faunadb - save email when signin

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
+import { query as q } from 'faunadb'
+
 
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,6 +13,37 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 })
